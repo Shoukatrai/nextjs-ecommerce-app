e89e1a59d5195701d7be953832afaf9738ac32ad
shoukatrai <raishoukat033@gmail.com>
2025-09-13 01:16:15 +0500

    footer ui complete

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,9 +6,9 @@ const Footer = () => {
   return (
     <div className="py-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 bg-gray-100 text-sm mt-24">
       {/* TOP */}
-      <div className="flex justify-between gap-24 ">
+      <div className="flex flex-col md:flex-row justify-between gap-24 ">
         {/* LEFT */}
-        <div className="w-1/4 flex flex-col gap-8 ">
+        <div className="w-full md:w-1/2 lg:w-1/4 flex flex-col gap-8 ">
           <Link href="/">
             <div className="text-2xl tracking-wide">SrShop</div>
           </Link>
@@ -27,11 +27,76 @@ const Footer = () => {
           </div>
         </div>
         {/* CENTER */}
-        <div className="hidden lg:flex justify-between w-1/2"></div>
+        <div className="hidden lg:flex justify-between w-1/2">
+          <div className="flex flex-col justify-between">
+            <h1 className="font-medium text-lg">COMPANY</h1>
+            <div className="flex flex-col gap-6">
+              <Link href="">About us</Link>
+              <Link href="">Careers</Link>
+              <Link href="">Affialiates</Link>
+              <Link href="">Blog</Link>
+              <Link href="">Contact us</Link>
+            </div>
+          </div>
+          <div className="flex flex-col justify-between">
+            <h1 className="font-medium text-lg">SHOP</h1>
+            <div className="flex flex-col gap-6">
+              <Link href="">About us</Link>
+              <Link href="">Careers</Link>
+              <Link href="">Affialiates</Link>
+              <Link href="">Blog</Link>
+              <Link href="">Contact us</Link>
+            </div>
+          </div>
+          <div className="flex flex-col justify-between">
+            <h1 className="font-medium text-lg">HELP</h1>
+            <div className="flex flex-col gap-6">
+              <Link href="">About us</Link>
+              <Link href="">Careers</Link>
+              <Link href="">Affialiates</Link>
+              <Link href="">Blog</Link>
+              <Link href="">Contact us</Link>
+            </div>
+          </div>
+        </div>
         {/* RIGHT */}
-        <div className="w-1/4"></div>
+        <div className="w-full md:w-1/2 lg:w-1/4 flex flex-col gap-8">
+          <h1 className="font-medium text-lg">SUBSCRIBE</h1>
+          <p>
+            Be the first to get the latest news about promotions and much more!
+          </p>
+          <div className="flex">
+            <input
+              type="text"
+              placeholder="Email Address"
+              className="p-4 w-3/4"
+            />
+            <button className="w-1/4 bg-number text-white">JOIN</button>
+          </div>
+          <span className="font-semibold">Secure Payments</span>
+          <div className="flex justify-between">
+            <Image src="/discover.png" alt="" width={40} height={20} />
+            <Image src="/skrill.png" alt="" width={40} height={20} />
+            <Image src="/paypal.png" alt="" width={40} height={20} />
+            <Image src="/mastercard.png" alt="" width={40} height={20} />
+            <Image src="/visa.png" alt="" width={40} height={20} />
+          </div>
+        </div>
       </div>
       {/* BOTTOM */}
+      <div className="flex flex-col md:flex-row items-center justify-between gap-8 mt-16">
+        <div className="">@2025 SrShop</div>
+        <div className="flex flex-col gap-8 md:flex-row">
+          <div className="">
+            <span className="text-gray-500 mr-4">Language</span>
+            <span className="font-medium">United States | English</span>
+          </div>
+          <div className="">
+            <span className="text-gray-500 mr-4">Currency</span>
+            <span className="font-medium">$ USD</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
