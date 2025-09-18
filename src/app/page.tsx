466c4cb6b49b5e8b7e6b361f64c73f9aee8a091b
shoukatrai <raishoukat033@gmail.com>
2025-09-19 03:25:54 +0500

    dynamice product shown-2:39

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import ProductList from "@/components/ProductList";
 import CategoryList from "@/components/CategoryList";
 import Slider from "@/components/Slider";
-import { useEffect } from "react";
+import { Suspense, useEffect } from "react";
 import { useWixClient } from "@/hooks/useWixClient";
 import { wixClientServer } from "@/lib/wixClientServer";
 
@@ -17,15 +17,17 @@ const HomePage = async () => {
   //   getProducts();
   // }, [wixClient]);
 
-  const wixClient = await wixClientServer();
-  const res = await wixClient.products.queryProducts().find();
-  console.log(res);
   return (
     <div className="">
       <Slider />
       <div className="m-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
         <h1 className="text-2xl ">Featured Products</h1>
-        <ProductList />
+        <Suspense fallback={"loading"}>
+          <ProductList
+            categoryId={process.env.FEATURED_PRODUCTS_ID!}
+            limit={4}
+          />
+        </Suspense>
       </div>
       <div className="">
         <h1 className="m-24 text-2xl  px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 mb-12">
@@ -35,7 +37,7 @@ const HomePage = async () => {
       </div>
       <div className="m-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
         <h1 className="text-2xl ">New Products</h1>
-        <ProductList />
+        <ProductList categoryId={process.env.FEATURED_PRODUCTS_ID!} limit={4} />
       </div>
     </div>
   );
